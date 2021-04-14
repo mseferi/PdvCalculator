2a07d181ce8c509e7f3f39b630674818a0b2142c
mseferi <seferimateo1994@gmail.com>
2021-04-14 13:37:14 +0200

    Added a button(btnBrisi) for deleting item from list. Added code for button(btnIzracunaj) wich on click adds new item on top of the list and hides keyboard. List being saved in SharedPreferences and loaded onCreate.

--- app/src/main/java/com/example/matopdv/MainActivity.java
+++ app/src/main/java/com/example/matopdv/MainActivity.java
@@ -1,9 +1,16 @@
 package com.example.matopdv;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +18,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.io.Serializable;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,9 +26,10 @@ public class MainActivity extends AppCompatActivity {
     public static final String KEY_DATA = "KEY_DATA";
     MyRecyclerView adapter;
 
-    private EditText etBroj, etPdv;
+    private EditText etbaseNumber, etvatAmount;
     private TextView tvPlus, tvPosto, tvJednako;
     private Button btnIzracunaj;
+    ArrayList<CalculationEntry> data = new ArrayList<>();
 
 
     @Override
@@ -28,28 +37,86 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        EditText etBroj = findViewById(R.id.etBroj);
-        EditText etPdv = findViewById(R.id.etPdv);
+        loadData();
+
+        EditText etBroj = findViewById(R.id.etbaseNumber);
+        EditText etPdv = findViewById(R.id.etvatAmount);
         TextView tvPlus = findViewById(R.id.tvPlus);
         TextView tvPosto = findViewById(R.id.tvPosto);
         TextView tvJednako = findViewById(R.id.tvJednako);
         Button btnIzracunaj = findViewById(R.id.btnIzracunaj);
 
 
-        ArrayList<ListItem> data = new ArrayList<>();
+
 
         if (savedInstanceState != null && savedInstanceState.containsKey(KEY_DATA)) {
-            data = (ArrayList<ListItem>) savedInstanceState.getSerializable(KEY_DATA);
+            data = (ArrayList<CalculationEntry>) savedInstanceState.getSerializable(KEY_DATA);
         } else {
-            data.add(new ListItem("", "", "", "", "", "", "", "", "", ""));
+            btnIzracunaj.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (etBroj.length() == 0) {
+                        etBroj.requestFocus();
+                        etBroj.setError(" Molimo unesite vrijednost");
+                    } else if ((etPdv.length() == 0)) {
+                        etPdv.requestFocus();
+                        etPdv.setError("Unesite iznos PDV-a");
+
+
+                    } else {
+                        double input = Double.parseDouble((etBroj.getText().toString()));
+                        double vatAmount = Double.parseDouble(etPdv.getText().toString());
+                        double pdvPosto = vatAmount / 100;
+                        CalculationEntry entry = new CalculationEntry(input, vatAmount, "ss");
+                        adapter.getmData().add(0,entry); //dodaje na vrh liste
+                        adapter.notifyDataSetChanged();
+
+
+
+                    }
+
+
+                    //Hides keyboard on button click
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(btnIzracunaj.getWindowToken(), InputMethodManager.RESULT_UNCHANGED_SHOWN);
+
+                }
+            });
+
+
+
 
         }
+
+
+
 
         adapter = new MyRecyclerView(this);
         adapter.setmData(data);
         RecyclerView rvData = findViewById(R.id.rV);
         rvData.setAdapter(adapter);
         rvData.setLayoutManager(new LinearLayoutManager(this));
+
+
+    }
+
+    private void saveData() {
+        SharedPreferences sharedPreferences = getSharedPreferences("shared preferences", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        Gson gson = new Gson();
+        String json = gson.toJson(data);
+        editor.putString("task list", json);
+        editor.apply();
+    }
+    private void loadData() {
+        SharedPreferences sharedPreferences = getSharedPreferences("shared preferences", MODE_PRIVATE);
+        Gson gson = new Gson();
+        String json = sharedPreferences.getString("task list", null);
+        Type type = new TypeToken<ArrayList<CalculationEntry>>() {}.getType();
+        data = gson.fromJson(json, type);
+        if (data == null) {
+            data = new ArrayList<>();
+        }
     }
 
 
@@ -57,5 +124,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
         outState.putSerializable(KEY_DATA, (Serializable) adapter.getmData());
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        saveData();
     }
 }
--- app/src/main/java/com/example/matopdv/MyRecyclerView.java
+++ app/src/main/java/com/example/matopdv/MyRecyclerView.java
@@ -1,6 +1,7 @@
 package com.example.matopdv;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,20 +14,21 @@ import java.util.List;
 
 public class MyRecyclerView extends RecyclerView.Adapter<MyRecyclerView.ViewHolder> {
 
-    private List<ListItem> mData;
+    private List<CalculationEntry> mData;
     private LayoutInflater mInflater;
     private ItemClickListener mClickListener;
+
 
     // data is passed into the constructor
     MyRecyclerView(Context context) {
         this.mInflater = LayoutInflater.from(context);
     }
 
-    public List<ListItem> getmData() {
+    public List<CalculationEntry> getmData() {
         return mData;
     }
 
-    public void setmData(List<ListItem> mData) {
+    public void setmData(List<CalculationEntry> mData) {
         this.mData = mData;
         notifyDataSetChanged();
     }
@@ -41,20 +43,26 @@ public class MyRecyclerView extends RecyclerView.Adapter<MyRecyclerView.ViewHold
     // binds the data to the TextView in each row
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-        ListItem item = mData.get(position);
-        holder.tvBroj2.setText(item.getPlus2());
-        holder.tvPlus2.setText(item.getPlus2());
-        holder.tvPdv2.setText(item.getPdv2());
-        holder.tvJednako2.setText(item.getJednako2());
-        holder.tvUkupno1.setText(item.getUkupno1());
-        holder.tvZagrada1.setText(item.getZagrada1());
-        holder.tvUkupno2.setText(item.getUkupno2());
-        holder.tvZagrada2.setText(item.getZagrada2());
-        holder.tvDatum.setText(item.getTvDatum());
+        CalculationEntry item = mData.get(position);
+        holder.tvBaseNumber.setText(String.format("%.2f", item.getFormattedBaseNumber()).replace('.', ','));
+        holder.tvVatAmount.setText(String.valueOf(item.getVatAmount()));
+        holder.tvdateCreated.setText(item.getFormattedDate());
+        holder.tvdateCreated.setTextColor(Color.GRAY);
+        holder.tvUkupno1.setText(String.format("%.2f", item.getTotalAmount()).replace('.', ','));
+        holder.tvUkupno2.setText(String.format("%.3f", item.getTotalAmount()).replace('.', ','));
 
-
-
+        holder.btnBrisi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CalculationEntry theRemovedItem = mData.get(position);
+                // remove your item from data base
+                mData.remove(position);  // remove the item from list
+                notifyItemRemoved(position); // notify the adapter about the removed item
+                notifyDataSetChanged();
+            }
+        });
     }
+
 
     // total number of rows
     @Override
@@ -65,29 +73,29 @@ public class MyRecyclerView extends RecyclerView.Adapter<MyRecyclerView.ViewHold
 
     // stores and recycles views as they are scrolled off screen
     public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-        TextView tvBroj2;
+        TextView tvBaseNumber;
         TextView tvPlus2;
-        TextView tvPdv2;
+        TextView tvVatAmount;
         TextView tvJednako2;
         TextView tvUkupno1;
         TextView tvZagrada1;
         TextView tvUkupno2;
         TextView tvZagrada2;
-        TextView tvDatum;
+        TextView tvdateCreated;
         Button btnBrisi;
 
 
         ViewHolder(View itemView) {
             super(itemView);
-            tvBroj2 = itemView.findViewById(R.id.tvBroj2);
+            tvBaseNumber = itemView.findViewById(R.id.tvBaseNumber);
             tvPlus2 = itemView.findViewById(R.id.tvPlus2);
-            tvPdv2 = itemView.findViewById(R.id.tvPdv2);
+            tvVatAmount = itemView.findViewById(R.id.tvVatAmount);
             tvJednako2 = itemView.findViewById(R.id.tvJednako2);
             tvUkupno1 = itemView.findViewById(R.id.tvUkupno1);
             tvUkupno2 = itemView.findViewById(R.id.tvUkupno2);
             tvZagrada1 = itemView.findViewById(R.id.tvZagrada1);
             tvZagrada2 = itemView.findViewById(R.id.tvZagrada2);
-            tvDatum = itemView.findViewById(R.id.tvDatum);
+            tvdateCreated = itemView.findViewById(R.id.tvDateCreated);
             btnBrisi = itemView.findViewById(R.id.btnBrisi);
             itemView.setOnClickListener(this);
         }
@@ -100,7 +108,7 @@ public class MyRecyclerView extends RecyclerView.Adapter<MyRecyclerView.ViewHold
     }
 
     // convenience method for getting data at click position
-    ListItem getItem(int id) {
+    CalculationEntry getItem(int id) {
         return mData.get(id);
     }
 
